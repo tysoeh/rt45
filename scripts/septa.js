86c54d1e6618bc49b327bff5b39e186b8e4de7f5
Ty Soehngen <soehngenm1@UOIFPTyoehngen5.fios-router.home>
2016-03-29 21:56:31 -0400

    solved

--- scripts/septa.js
+++ scripts/septa.js
@@ -13,8 +13,8 @@ app.factory('routeTrace', ['$http', function($http) {
 }]).factory('northSched', ['$http', function($http) {
     return $http({
           method: 'JSONP',
-          url: 'http://www3.septa.org/hackathon/BusSchedules/?req1=16608&req2=45&req3=i&req6=4',
-          withCredentials: true
+          // http://blog.novanet.no/angularjs-with-jsonp-and-how-i-get-to-work-on-time/
+          url: 'http://www3.septa.org/hackathon/BusSchedules/?req1=16608&req2=45&req3=i&req6=4&callback=JSON_CALLBACK',
         })
         .success(function(data) {
             return (data);
@@ -23,7 +23,10 @@ app.factory('routeTrace', ['$http', function($http) {
             return (err);
         });
 }]).factory('southSched', ['$http', function($http) {
-    return $http.get('http://www3.septa.org/hackathon/BusSchedules/?req1=16498&req2=45&req3=i&req6=4')
+    return $http({
+          method: 'JSONP',
+          url: 'http://www3.septa.org/hackathon/BusSchedules/?req1=16498&req2=45&req3=i&req6=4&callback=JSON_CALLBACK',
+        })
         .success(function(data) {
             return (data);
         })
@@ -31,7 +34,10 @@ app.factory('routeTrace', ['$http', function($http) {
             return (err);
         });
 }]).factory('locations', ['$http', function($http) {
-    return $http.get('http://www3.septa.org/hackathon/TransitView/?route=45')
+    return $http({
+          method: 'JSONP',
+          url: 'http://www3.septa.org/hackathon/TransitView/?route=45&callback=JSON_CALLBACK',
+        })
         .success(function(data) {
             return (data);
         })
@@ -40,7 +46,10 @@ app.factory('routeTrace', ['$http', function($http) {
         });
 
 }]).factory('detours', ['$http', function($http) {
-    return $http.get('http://www3.septa.org/hackathon/BusDetours/?req1=45')
+    return $http({
+          method: 'JSONP',
+          url: 'http://www3.septa.org/hackathon/BusDetours/?req1=45&callback=JSON_CALLBACK',
+        })
         .success(function(data) {
             return (data);
         })
